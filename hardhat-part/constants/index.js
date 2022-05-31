e7ea5a3d78130346adbab579e2c5fb8c881f65d5
emremre <emre.guclu@metu.edu.tr>
2022-05-31 04:35:43 +0300

    initial commit

---
+++ hardhat-part/constants/index.js
@@ -0,0 +1,8 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = '0x6A58BcF640d3A8DcaEF47f41cA045E88238430A5';
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
+
+
+FakeNFTMarketplace deployed to:  0x5B45ff230522C88B405190c3ac2F014b752b253c
+CryptoDevsDAO deployed to:  0x87D9cc65Cbec707748Ae28E96b0CFeA6a275014c
